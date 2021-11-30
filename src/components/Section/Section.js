8b1f45124223d22ec27db45622958f7aa0fc13e7
Fosa1990 <fosa1990@gmail.com>
2021-11-30 12:12:24 +0200

    Create Section.js

---
+++ src/components/Section/Section.js
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const SectionWrapper = styled.section`
+  padding: 10px 25px 10px 25px;
+  margin: 0 25px 5px 25px;
+`;
+const Title = styled.h2`
+  font-size: 32px;
+  font-family: 'Roboto', sans-serif;
+`;
+
+const Section = ({ title, children }) => {
+  return (
+    <SectionWrapper>
+      <Title>{title}</Title>
+      {children}
+    </SectionWrapper>
+  );
+};
+
+Section.propTypes = {
+  title: PropTypes.string,
+  children: PropTypes.node,
+};
+
+export default Section;
