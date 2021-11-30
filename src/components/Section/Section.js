2cc1de38a0864434afcb1bfd5627081cad45573d
Fosa1990 <fosa1990@gmail.com>
2021-11-30 22:34:03 +0200

    Update Section.js

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -6,56 +6,29 @@ const SectionWrapper = styled.section`
     props.flex
       ? 'display: flex; align-items: center; justify-content: center; flex-direction: row;'
       : 'display: block;'};
+
   ${props => (props.vertical ? 'flex-direction: column;' : '')};
 
   padding: 0 25px;
   margin: 0;
 `;
-const MainTitle = styled.h1`
-  text-align: center;
-  font-size: 32px;
-  font-family: 'Roboto', sans-serif;
-  color: var(--yellow);
-`;
-const Title = styled.h2`
-  text-align: center;
-  font-size: 32px;
-  font-family: 'Roboto', sans-serif;
-`;
 
-const IsItMainTitle = ({ mainTitle, title }) => {
-  return (
-    <>
-      {mainTitle === true ? (
-        <MainTitle>{title}</MainTitle>
-      ) : (
-        <Title>{title}</Title>
-      )}
-    </>
-  );
-};
-
-const Section = ({ title, mainTitle, children, flex, vertical }) => {
+const Section = ({ children, flex, vertical }) => {
   return (
     <SectionWrapper flex={flex} vertical={vertical}>
-      <IsItMainTitle title={title} mainTitle={mainTitle} />
       {children}
     </SectionWrapper>
   );
 };
 
 Section.defaultProps = {
-  mainTitle: false,
-  title: 'Section title',
   flex: false,
   vertical: false,
 };
 
 Section.propTypes = {
-  mainTitle: PropTypes.bool,
-  title: PropTypes.string,
   children: PropTypes.node,
-  verticalFlex: PropTypes.bool,
+  vertical: PropTypes.bool,
 };
 
 export default Section;
