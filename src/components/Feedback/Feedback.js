a77b888f1a8a8abf902f06dfdfbc19ce0cc5f255
Fosa1990 <fosa1990@gmail.com>
2021-11-30 12:11:50 +0200

    Create Feedback.js

---
+++ src/components/Feedback/Feedback.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import Section from '../Section';
+import Statistics from '../Statistics';
+import FeedbackOptions from '../FeedbackOptions';
+
+export class Feedback extends Component {
+  static deafaultProps = {
+    initialValue: 0,
+  };
+
+  state = {
+    good: this.props.initialValue,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleClickBtnGood = () => {
+    this.setState(prevState => {
+      return {
+        good: prevState.good + 1,
+      };
+    });
+    this.countTotalFeedback();
+    this.countPositiveFeedbackPercentage();
+  };
+
+  handleClickBtnNeutral = () => {
+    this.setState(prevState => {
+      return {
+        neutral: prevState.neutral + 1,
+      };
+    });
+    this.countTotalFeedback();
+    this.countPositiveFeedbackPercentage();
+  };
+
+  handleClickBtnBad = () => {
+    this.setState(prevState => {
+      return {
+        bad: prevState.bad + 1,
+      };
+    });
+    this.countTotalFeedback();
+    this.countPositiveFeedbackPercentage();
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    return total ? Math.floor((good / total) * 100) : 0;
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const percent = this.countPositiveFeedbackPercentage();
+
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            onHandleClickBtnGood={this.handleClickBtnGood}
+            onHandleClickBtnNeutral={this.handleClickBtnNeutral}
+            onHandleClickBtnBad={this.handleClickBtnBad}
+          />
+        </Section>
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            percent={percent}
+          />
+        </Section>
+      </>
+    );
+  }
+}
+
+export default Feedback;
